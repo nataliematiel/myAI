0c46d927c38986d05d575a0e239879a4bbe677f2
nataliematiel <Nataliematiel@gmail.com>
2025-02-19 14:00:58 -0500

    updated owned by

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -5,6 +5,6 @@ export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
 export const FOOTER_MESSAGE: string = `[Customized AI by Natalie Maloney]`;
 export const CLEAR_BUTTON_TEXT: string = `[CLEAR BUTTON TEXT]`;
 export const PAGE_TITLE: string = `[PAGE TITLE]`;
-export const PAGE_DESCRIPTION: string = `Chat with ${Nat Dawg}, ${Natalie Maloneys}'s AI assistant.`;
+export const PAGE_DESCRIPTION: string = `Chat with ${Nat Dawg}, ${Natalie}'s AI assistant.`;
 
 export const EMPTY_CITATION_MESSAGE: string = "Unspecified source";

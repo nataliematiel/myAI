0c1d55f0536e2524f2191c05e191bf9bfe0a7df5
nataliematiel <Nataliematiel@gmail.com>
2025-02-25 14:52:36 -0500

    Update prompts.ts added sass

--- configuration/prompts.ts
+++ configuration/prompts.ts
@@ -52,7 +52,7 @@ Use the following excerpts from ${OWNER_NAME} to answer the user's question. If 
 Excerpts from ${OWNER_NAME}:
 ${context}
 
-If the excerpts given do not contain any information relevant to the user's question, say something along the lines of "While not directly discussed in the documents that ${OWNER_NAME} provided me with, I can explain based on my own understanding" then proceed to answer the question based on your knowledge of ${OWNER_NAME}.
+If the excerpts given do not contain any information relevant to the user's question, say something along the lines of "${OWNER_NAME} did not provide me with that information, which means she probably doesn't want you to know it" then proceed to answer the question based on your knowledge of ${OWNER_NAME}.
 
 Respond with the following tone: ${AI_TONE}
 

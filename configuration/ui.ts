74d08a230fa362851994eb15ef330d5b930f9102
nataliematiel <Nataliematiel@gmail.com>
2025-02-27 11:32:05 -0500

    Update ui.ts

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -3,7 +3,7 @@ import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 export const CHAT_HEADER: string = `Nat Dawg`;
 export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
 export const FOOTER_MESSAGE: string = `Customized AI by Natalie Maloney`;
-export const CLEAR_BUTTON_TEXT: string = `Start New]`;
+export const CLEAR_BUTTON_TEXT: string = `Start New`;
 export const PAGE_TITLE: string = `Nat Dawg`;
 export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
 

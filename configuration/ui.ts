355f51e923a5e77699bcab63c3929df652dc95ad
nataliematiel <Nataliematiel@gmail.com>
2025-02-27 11:28:54 -0500

    Update ui.ts page header

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -1,6 +1,6 @@
 import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
-export const CHAT_HEADER: string = `[Nat Dawg]`;
+export const CHAT_HEADER: string = `Nat Dawg`;
 export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
 export const FOOTER_MESSAGE: string = `[Customized AI by Natalie Maloney]`;
 export const CLEAR_BUTTON_TEXT: string = `[CLEAR BUTTON TEXT]`;
